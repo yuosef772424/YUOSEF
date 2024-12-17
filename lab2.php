bdc796167cd96911bc807bc4553fd7da57d83502
yosef772424 <yosef772424@gmail.com>
2024-12-17 23:17:15 +0300

    second assignment

--- lab2.php
+++ lab2.php
@@ -1,50 +1,111 @@
-
-
 <?php
+
 function p($n){
-    echo $n."<br>";
+    if (is_array($n)){
+        foreach ($n as $x){
+            p($x);
+        }}
+    else{
+    echo $n."<br>";}
 }
+$arr1 = array(1, 2, 3);
+$arr2 = [1, 2, 3];
 
-$x = 10;
-$y = 15;
-if($x < $y):           // تصحيح $X إلى $x (حساسية حالة الأحرف)
-    p( "the $x < $y");
-else:
-    p( "the $x > $y");
-endif;           // إضافة endif لإغلاق جملة if
+$person = [
+    "name" => "Yousef", 
+    "age" => 25
+];
 
-$yuosef = "yuosef mansor";
-$len = strlen($yuosef); 
-echo $len; // تصحيح $yosef إلى $yuosef
-for ($i = 0; $i < $len-1; $i++) { 
-    p( "I " . ($i+1) . " " . substr($yuosef, 0, $i+1) . "<br>");    
-}
-$i=3;
-switch($i) {
-    case 0:
-        p( $i);
-        break;
-    case 2:
-        p( "case 2");
-    default:
-        p( "the value out range");
+// Array operations
+array_push($arr1, "new_item");
+p($arr1);
 
-}
-$x=10;
-while($x<10){
-    p("the loop $x");
-    $x+=1;
-}
-do{
-    p( "yuosef");
-    $x+=1;
-}while($x<10);
+array_unshift($arr2, "new_item"); 
+p($arr2);
 
-$y="yuosef";
+array_pop($arr1);
+p($arr1);
 
-$y = "yuosef";
-$n = ["item1", "item2", "item3"];  
+array_shift($arr2);
+p($arr2);
 
-foreach($n as $y){
-    p($y . '</br>');  
-}
+// Array functions
+p(count($arr1));
+p(in_array("item", $arr1));
+p(array_search("item", $arr1));
+
+// Sorting
+sort($arr1);
+p($arr1);
+
+rsort($arr1);
+p($arr1);
+
+asort($arr1);
+p($arr1);
+
+ksort($arr1);
+p($arr1);
+
+// Array transformations
+$merged = array_merge($arr1, $arr2);
+p($merged);
+
+$filtered = array_filter($arr1, function($item) {
+    return $item > 5;
+});
+p($filtered);
+
+$mapped = array_map(function($item) {
+    return $item * 2;
+}, $arr1);
+p($mapped);
+
+$joined = implode(",", $arr1);
+p($joined);
+
+$unique = array_unique($arr1);
+p($unique);
+$str = "Hello World";
+// String functions
+p(strlen($str));
+p(strtoupper($str));
+p(strtolower($str));
+p(ucfirst($str));
+p(ucwords($str));
+
+// String search and manipulation
+p(strpos($str, "search"));
+p(str_replace("old", "new", $str));
+p(substr($str, 0, 5));
+
+// String trimming
+p(trim($str));
+p(ltrim($str));
+p(rtrim($str));
+
+// String splitting
+p(explode(" ", $str));
+
+// String searching
+p(substr_count($str, "search"));
+p(strstr($str, "search"));
+p(stristr($str, "search"));
+$str1 = "Test String 1";
+$str2 = "Test String 2";
+// String comparison
+p(strcmp($str1, $str2));
+p(strcasecmp($str1, $str2));
+
+// Hashing
+p(md5($str));
+p(sha1($str));
+$name = "Yousef";
+$age = 25;
+
+// Formatting
+p(printf("Name: %s, Age: %d", $name, $age));
+p(sprintf("Name: %s, Age: %d", $name, $age));
+
+// String repetition and padding
+p(str_repeat($str, 3));
